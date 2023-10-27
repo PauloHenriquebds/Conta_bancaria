9608c3064822d2ec2c74aa6106e34d93efdd0a60
PauloHenriquebds <145726119+PauloHenriquebds@users.noreply.github.com>
2023-10-27 16:30:20 -0300

    Update conta bancaria.py

--- conta bancaria.py
+++ conta bancaria.py
@@ -1,9 +1,9 @@
 from biblioteca import ContaBancaria
 
-conta = ContaBancaria("Paulo", 0, 123321, "Corrente", 800, False)
+conta = ContaBancaria("Paulo", 123321, "Corrente", False)
 
 conta.ativarconta()
+conta.ativarlimite()
 conta.depositar()
 conta.sacar()
-conta.ativarconta()
 conta.verificarsaldo()
